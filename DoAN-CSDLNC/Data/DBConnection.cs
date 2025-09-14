a6ec6e495d96c33eecd1bb06ce86346c2d8be1c0
PhucTran <157440074+LiviasWS@users.noreply.github.com>
2025-09-14 23:27:11 +0700

    add db context

---
+++ DoAN-CSDLNC/Data/DBConnection.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAN_CSDLNC.Data
+{
+    public class DBConnection
+    {
+        private readonly IMongoDatabase _database;
+        private readonly MongoClient _client;
+        private readonly string _connectionString = "";
+        private readonly string _databaseName = "";
+
+        public DBConnection()
+        {
+            _client = new MongoClient(_connectionString);
+            _database = _client.GetDatabase(_databaseName);
+        }
+
+        public IMongoDatabase GetDatabase()
+        {
+            return _database;
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+}
